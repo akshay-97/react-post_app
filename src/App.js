1a4ad8f0b1653db2413293cb545b68bd218ceeb1
Daanvirsingh <daanvirsingh2@gmail.com>
2019-06-29 15:28:09 +0530

    navbar corrected

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar';
-import AddPost from './AddPost';
-import Posts from './Posts';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+import AddPost from "./AddPost";
+import Posts from "./Posts";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: []
+    };
+  }
+
+  addPost = (a,b) => {
+
+  };
+
   render() {
     return (
       <div>
-        <Navbar/>
-        <AddPost/>
-        <Posts/>
+        <Navbar />
+        <AddPost addPost={this.addPost}/>
+        <Posts post={this.state.input}/>
+
       </div>
     );
   }
 }
 
 export default App;
-
